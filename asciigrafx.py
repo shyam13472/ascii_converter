adca5fe4c7eed4ea20533585812f66806dcdc33f
TheSwagVader <egorzverev666@gmail.com>
2023-02-16 10:38:53 +0300

    Added argparse support

--- asciigrafx.py
+++ asciigrafx.py
@@ -5,6 +5,7 @@ import os
 import sys
 import subprocess
 import shutil
+import argparse
 
 DEFAULT_EXTRACTION_FPS = 30
 DEFAULT_ASCII_CHAR_HEIGHT = 5
@@ -13,42 +14,47 @@ DEFAULT_TEXTUREPACK_INDEX = 0
 
 
 def main():
-    
-    if len(sys.argv) < 2:
-        print('Usage: python3 asciigrafx.py <path-to-videofile>')
-    else:
-        video_file = sys.argv[1]
+    appArgs = argparse.ArgumentParser(
+        description='ASCIIGRAFX graphic converter. SHEG Enterprises Incorporated. All rights reserved.\nProcesses a video in a video in ASCII graphics.'
+    )
+    appArgs.add_argument('videoToProcess', type=str, help='A videofile to process into ASCII graphics')
 
-        print('Welcome to ASCIIGRAFX Converter\nSHEG Enterprises Inc., All rights reserved\nUses ffmpeg ver. 5.1.2.\n')
-        print(f'Videofile path: {video_file}')
+    args = appArgs.parse_args()
+    #if len(sys.argv) < 2:
+    #    print('Usage: python3 asciigrafx.py <path-to-videofile>')
+    #else:
+    video_file = args.videoToProcess
 
-        print('Extracting configfile...', end = '')
-        conf = ASCIIGRAFXConfigFile('config')
-        params = conf.getConfiguration()
-        print('done')
+    print('Welcome to ASCIIGRAFX Converter\nSHEG Enterprises Inc., All rights reserved\nUses ffmpeg ver. 5.1.2.\n')
+    print(f'Videofile path: {video_file}')
 
-        texturepackIndex = DEFAULT_TEXTUREPACK_INDEX if params['texturepack-index'] == 'def' else int(params['texturepack-index'])
-        extractionFPS = DEFAULT_EXTRACTION_FPS if params['extraction-fps'] == 'def' else int(params['extraction-fps'])
-        asciiCharHeight = DEFAULT_ASCII_CHAR_HEIGHT if params['ascii-pixel-char-height'] == 'def' else int(params['ascii-pixel-char-height'])
-        print('Conversion parameters:')
-        print('Video extraction FPS: %s\nASCII char height: %s\nTexturepack index of textures.txt %s' % (extractionFPS, asciiCharHeight, texturepackIndex))
+    print('Extracting configfile...', end = '')
+    conf = ASCIIGRAFXConfigFile('config')
+    params = conf.getConfiguration()
+    print('done')
 
-        print('Extracting texturepack from textures.txt...', end='')
-        with open('textures.txt' , 'r', encoding='utf8') as fitextures:
-            texturepack = fitextures.readlines()[texturepackIndex]
-        print('done')
+    texturepackIndex = DEFAULT_TEXTUREPACK_INDEX if params['texturepack-index'] == 'def' else int(params['texturepack-index'])
+    extractionFPS = DEFAULT_EXTRACTION_FPS if params['extraction-fps'] == 'def' else int(params['extraction-fps'])
+    asciiCharHeight = DEFAULT_ASCII_CHAR_HEIGHT if params['ascii-pixel-char-height'] == 'def' else int(params['ascii-pixel-char-height'])
+    print('Conversion parameters:')
+    print('Video extraction FPS: %s\nASCII char height: %s\nTexturepack index of textures.txt %s' % (extractionFPS, asciiCharHeight, texturepackIndex))
 
-        print(f'Loaded texturepack: {texturepack}')
+    print('Extracting texturepack from textures.txt...', end='')
+    with open('textures.txt' , 'r', encoding='utf8') as fitextures:
+        texturepack = fitextures.readlines()[texturepackIndex]
+    print('done')
 
-        print('Beginning the conversion!')
+    print(f'Loaded texturepack: {texturepack}')
 
-        carver(video_file, extractionFPS)
-        conventers(asciiCharHeight, texturepack)
-        subprocess.check_output(["C:\\Users\\EgrZver\\Documents\\ffmpeg-5.1.2-full_build\\bin\\ffmpeg.exe", "-framerate", str(extractionFPS), "-i", f"{os.getcwd()}/processed/%07d.jpg", f"{video_file}-{extractionFPS}fps-{asciiCharHeight}h-processed.mp4"])
-        shutil.rmtree("not_processed/")
-        shutil.rmtree('processed/')
+    print('Beginning the conversion!')
 
-        print(f'Done! Output file is {video_file}-{extractionFPS}fps-{asciiCharHeight}h-processed.mp4')
+    carver(video_file, extractionFPS)
+    conventers(asciiCharHeight, texturepack)
+    subprocess.check_output(["C:\\Users\\EgrZver\\Documents\\ffmpeg-5.1.2-full_build\\bin\\ffmpeg.exe", "-framerate", str(extractionFPS), "-i", f"{os.getcwd()}/processed/%07d.jpg", f"{video_file}-{extractionFPS}fps-{asciiCharHeight}h-processed.mp4"])
+    shutil.rmtree("not_processed/")
+    shutil.rmtree('processed/')
+
+    print(f'Done! Output file is {video_file}-{extractionFPS}fps-{asciiCharHeight}h-processed.mp4')
     
     
 
